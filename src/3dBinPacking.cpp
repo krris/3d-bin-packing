2380c6af6bdcbfbaa18efc28dcfebf1cb0baef2b
Krris <krrisgit@gmail.com>
2013-05-30 23:40:03 +0200

    Fixed: missing app directory.

--- src/3dBinPacking.cpp
+++ src/3dBinPacking.cpp
@@ -3,7 +3,6 @@
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
